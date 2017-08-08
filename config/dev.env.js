0f9b8fb66e6261250c08af661fdbfe073d83837a
xogost <xogost@gmail.com>
2017-08-07 20:37:23 -0500

    delete env vars from config file for dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,11 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  CLIENT_ID: '',
-  CLIENT_DOMAIN: '',
-  REDIRECT: '',
-  SCOPE: '',
-  AUDIENCE: '',
-  BASE_URL: ''
+  NODE_ENV: '"development"'
 })
