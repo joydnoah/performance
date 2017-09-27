a78b9cfc4a6cb44c84e90d86dcbbbd6c3434150f
xogost <xogost@gmail.com>
2017-09-26 21:54:16 -0500

    update based on comments in PR #28

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,5 +8,9 @@ module.exports = merge(prodEnv, {
   AUTH0_CALLBACK_REDIRECT: '"' + process.env.AUTH0_CALLBACK_REDIRECT + '"',
   SCOPE: '"full_access"',
   AUTH0_AUDIENCE: '"' + process.env.AUTH0_AUDIENCE + '"',
-  JOBS_API_URL: '"' + process.env.JOBS_API_URL + '"'
+  JOBS_API_URL: '"' + process.env.JOBS_API_URL + '"',
+  LINKEDIN_CLIENT_ID: '"' +  process.env.LINKEDIN_CLIENT_ID + '"',
+  LINKEDIN_CALLBACK: '"' +  process.env.LINKEDIN_CALLBACK + '"',
+  GOOGLE_API_KEY: '"' +  process.env.GOOGLE_API_KEY + '"',
+  FACEBOOK_APP_ID: '"' + process.env.FACEBOOK_APP_ID + '"'
 })
