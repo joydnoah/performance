da60ba3f9f07f091ad2a298116bd4f1a9b0bca67
Sebastian <tykiblood1992@hotmail.com>
2017-12-09 00:40:37 -0500

    - Changed ID column to be the position_number column of the data base

--- src/components/Positions.vue
+++ src/components/Positions.vue
@@ -94,7 +94,7 @@
           <div class="row">
             <div class="col-xs-1">
               <p class="row-info row-title">
-                {{ item.id }}
+                {{ item.position_number }}
               </p>
             </div>
             <div class="col-xs-2">
