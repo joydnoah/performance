111059494a65d993df2b51ae36fd8030d7a7ba95
Sebastian <tykiblood1992@hotmail.com>
2018-01-07 02:09:48 -0500

    - New UI implemented

--- src/components/EmailTemplates.vue
+++ src/components/EmailTemplates.vue
@@ -1,37 +1,65 @@
 <template>
   <div id="general-container">
-    <app-nav></app-nav>
-    <div id="container-positions" class="panel panel-default">
-      <div class="panel-heading"><h3>Posición {{ position.name }} - Correos electrónicos automatizados</h3></div>
-      <div class="panel-body">
-        <tabs :justified="true">
-          <tab title="<i class='glyphicon glyphicon-ok'></i> Agradecer solicitud" :html-title="true">
-            <email-control-template :position_id="position_id" :type_prop="'in_process'"></email-control-template>
-          </tab>
-          <tab title="<i class='glyphicon glyphicon-thumbs-down'></i> Rechazar Candidato" :html-title="true">
-            <email-control-template :position_id="position_id" :type_prop="'reject'"></email-control-template>
-          </tab>
-          <tab title="<i class='glyphicon glyphicon-phone-alt'></i> Invitar Candidato a LLamada" :html-title="true">
-            <email-control-template :position_id="position_id" :type_prop="'scheduled_call'"></email-control-template>
-          </tab>
-          <tab title="<i class='glyphicon glyphicon-thumbs-up'></i> Invitar Candidato a Entrevista" :html-title="true">
-            <email-control-template :position_id="position_id" :type_prop="'scheduled_interview'"></email-control-template>
-          </tab>
-        </tabs>
+    <toolbar></toolbar>
+    <!-- header start -->
+    <div class="section-header">
+      <div class="general-container">
+        <div class="row">
+          <div class="col-xs-12">
+            <h1 class="header-title wow fadeInLeft" data-wow-delay="0.4s" data-wow-duration="1.2s">Correos electrónicos automatizados</h1>
+            <span class="header-title-message wow fadeInLeft" data-wow-delay="0.4s" data-wow-duration="1.2s">Posición {{ position.name }}</span>
+          </div>
+        </div>
+      </div>
+    </div><!-- header end -->
+
+    <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+
+      <!-- <div class=""></div> -->
+      <div class="email-tabs-container">
+        <div class="general-container">
+          <div class="row">
+            <div class="mdl-tabs__tab-bar">
+              <a href="#regards-template" class="mdl-tabs__tab is-active" @click=change_tab($event)>Agradecer solicitud</a>
+              <a href="#reject-template" class="mdl-tabs__tab">Rechazar candidato</a>
+              <a href="#call-template" class="mdl-tabs__tab">Invitar candidato a llamada</a>
+              <a href="#interview-template" class="mdl-tabs__tab">Invitar candidato a entrevista</a>
+            </div>
+          </div>
+        </div><!-- general-container end -->
+      </div><!-- email-tabs-container end -->
+
+      <div class="general-container">
+        <div class="row">
+          <div class="col-xs-offset-2 col-xs-8">
+            <div class="mdl-tabs__panel is-active" id="regards-template">
+              <email-control-template :position_id="position_id" :type_prop="'in_process'"></email-control-template>
+            </div>
+            <div class="mdl-tabs__panel" id="reject-template">
+              <email-control-template :position_id="position_id" :type_prop="'reject'"></email-control-template>
+            </div>
+            <div class="mdl-tabs__panel" id="call-template">
+              <email-control-template :position_id="position_id" :type_prop="'scheduled_call'"></email-control-template>
+            </div>
+            <div class="mdl-tabs__panel" id="interview-template">
+              <email-control-template :position_id="position_id" :type_prop="'scheduled_interview'"></email-control-template>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import AppNav from './AppNav'
+  import Toolbar from './Toolbar'
   import EmailControlTemplate from './EmailControlTemplate'
   import { getAccessToken, getIdToken, isLoggedIn } from '../../utils/auth'
   import { Tabs } from 'uiv'
-  
+
   export default {
     components: {
-      AppNav,
+      Toolbar,
       EmailControlTemplate,
       Tabs
     },
@@ -39,6 +67,7 @@
       return {
         position_id: this.$route.params.position_id,
         position: {},
+        bootstrap_min_js: null,
         config: {
           placeholderText: 'Edita tu contenido aquí!',
           events: {
@@ -51,9 +80,17 @@
     methods: {
       isLoggedIn () {
         return isLoggedIn()
+      },
+      change_tab (event) {
+        console.log(event)
       }
     },
     mounted: function () {
+      this.bootstrap_min_js = document.createElement('script')
+      this.bootstrap_min_js.setAttribute('src', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js')
+      this.bootstrap_min_js.setAttribute('integrity', 'sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa')
+      this.bootstrap_min_js.setAttribute('crossorigin', 'anonymous')
+      document.head.appendChild(this.bootstrap_min_js)
       this.axios.defaults.headers.common['Authorization'] = `Bearer ${getIdToken()}[${getAccessToken()}`
       this.axios.get('/position/' + this.position_id)
       .then((response) => {
@@ -63,6 +100,8 @@
     }
   }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style scoped>
   #container-positions{
