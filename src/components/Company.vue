7fdddece09e98307536f41381dace554e8fdb48f
Sebastian <tykiblood1992@hotmail.com>
2018-02-18 02:44:56 -0500

    - Change of UI for public company page

--- src/components/Company.vue
+++ src/components/Company.vue
@@ -1,37 +1,77 @@
 <template>
   <div id="general-container">
-    <div id="container-preview">
-      <div class="panel panel-default">
-        <div class="panel-body position-panel">
-          <p>
-            {{ company.description }}
-          </p>
-          <h3> Pocisiones abiertas actualmente:</h3>
-          <div class="form-group">
-            <label></label>
-            <select id="departments-list" class="form-control" v-model="department_to_filter" v-on:change="filter_department">
-              <option value="0">
-                Todos
-              </option>
-              <option v-for="item in department_list" v-bind:value="item.id">
-                <div> {{ item.name }} </div>
-              </option>
-            </select>
+    <div>
+      <div class="body-container">
 
-            <div v-for="item in department_list" class="department-container" v-bind:id="'department-' + item.id">
-              <div v-if="test_position(item)">
-                <h4>{{ item.name }}</h4>
-                <div v-for="item_position in positions">
-                  <div v-if="item_position.department_name === item.name">
-                    <a v-bind:href="'/position-apply/' + item_position.id">{{ item_position.name }} - {{ item_position.city }}</a>
+        <div class="general-container">
+          <div class="public-page-content">
+
+            <div class="row">
+              <div class="col-xs-offset-2 col-xs-8">
+                <div class="page-logo">
+                  <img :src="'/static/html_layout/images/logo-color.png'">
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-offset-2 col-xs-8">
+                <div class="page-title">Acerca de la empresa</div>
+              </div>
+              <div v-show="isEmpty( company.description )" class="col-xs-offset-2 col-xs-8">
+                <div class="page-item">
+                  <div class="item-label">Descripción de la empresa</div>
+                  <div class="item-description">{{ company.description }}</div>
+                </div>
+              </div>
+              <div v-show="false" class="col-xs-offset-2 col-xs-8">
+                <div class="page-item">
+                  <div class="item-label">Nombre de la empresa</div>
+                  <div class="item-description">{{ company.name }}</div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-xs-offset-2 col-xs-8">
+                <div class="page-title">Posiciones abiertas</div>
+              </div>
+              <div class="col-xs-offset-2 col-xs-8">
+                <div class="separator"></div>
+              </div>
+            </div>
+
+            <div v-for="item in department_list" v-bind:value="item.id">
+              <div v-if="testPosition(item)">
+                <div class="page-position-container">
+                  <div class="row">
+                    <div class="col-xs-offset-2 col-xs-3">
+                      <div class="position-title">{{ item.name }}</div>
+                    </div>
+                    <div class="col-xs-5">
+                      <div v-for="item_position in positions">
+                        <div v-if="item_position.department_name === item.name || (item_position.department_name == null && item.name === 'Otros')">
+                          <div class="position-item">
+                            <div class="item-label">{{ item_position.city }}</div>
+                            <div v-on:click="goTo( '/position-apply/' + item_position.id )" class="item-description">{{ item_position.name }}</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-xs-offset-2 col-xs-8">
+                    <div class="separator"></div>
                   </div>
                 </div>
               </div>
             </div>
 
-          </div>
-        </div>
-      </div>
+          </div><!-- create-form end -->
+        </div><!-- create-form-container end -->
+
+      </div><!-- body-container end -->
     </div>
   </div>
 </template>
@@ -53,19 +93,30 @@
       }
     },
     methods: {
+      goTo (url) {
+        window.location.href = url
+      },
       isLoggedIn () {
         return isLoggedIn()
       },
-      exit () {
+      isEmpty (str) {
+        return !!str
       },
-      test_position (position) {
+      testPosition (position) {
         for (var i = 0; i < this.positions.length; i++) {
-          if (this.positions[i].department_name === position.name) {
+          if (this.positions[i].department_name === position.name || (this.positions[i].department_name == null && position.name === 'Otros')) {
             return true
           }
         }
       },
-      get_positions () {
+      otherPositions () {
+        for (var i = 0; i < this.department_list.length; i++) {
+          if (!this.department_list[i].name) {
+            this.department_list[i].name = 'Otros'
+          }
+        }
+      },
+      getPositions () {
         this.axios.defaults.headers.common['Authorization'] = `Bearer ${getIdToken()}[${getAccessToken()}`
         this.axios.get('/company/' + this.$route.params.uri + '/positions')
         .then(response => {
@@ -77,44 +128,41 @@
         })
         .catch(error => { console.log(error.response) })
       },
-      get_departments () {
+      getDepartments () {
         this.axios.defaults.headers.common['Authorization'] = `Bearer ${getIdToken()}[${getAccessToken()}`
         this.axios.get('/departments/' + this.company.id)
         .then((response) => {
           this.department_list = response.data.data.departments
+          this.otherPositions()
         })
         .catch(error => {
           console.log(error.response)
         })
       },
-      get_company () {
+      getCompany () {
         this.axios.defaults.headers.common['Authorization'] = `Bearer ${getIdToken()}[${getAccessToken()}`
         this.axios.get('/company/uri/' + this.$route.params.uri)
         .then((response) => {
           this.company = response.data.data.company
-          this.get_departments()
+          this.getDepartments()
         })
         .catch(error => { console.log(error.response) })
-      },
-      filter_department () {
-        if (this.department_to_filter === '0') {
-          for (var i = 0; i < document.getElementsByClassName('department-container').length; i++) {
-            document.getElementsByClassName('department-container')[i].style.display = 'block'
-          }
-        } else {
-          for (var j = 0; j < document.getElementsByClassName('department-container').length; j++) {
-            document.getElementsByClassName('department-container')[j].style.display = 'none'
-          }
-          document.getElementById('department-' + this.department_to_filter).style.display = 'block'
-        }
       }
     },
     mounted () {
-      this.get_company()
-      this.get_positions()
+      this.getCompany()
+      this.getPositions()
+      this.bootstrap_min_js = document.createElement('script')
+      this.bootstrap_min_js.setAttribute('src', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js')
+      this.bootstrap_min_js.setAttribute('integrity', 'sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa')
+      this.bootstrap_min_js.setAttribute('crossorigin', 'anonymous')
+      document.head.appendChild(this.bootstrap_min_js)
     }
   }
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
 <style scoped>
     #container-preview{
       width: 90%;
