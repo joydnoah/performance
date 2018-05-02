54ade7a743d94877d3acb86b318926708b18ed18
John Roa <jhonjairoroa87@gmail.com>
2018-05-02 01:00:28 -0500

    Added client custom domain

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   CLIENT_ID: '"' + process.env.CLIENT_ID + '"',
   CLIENT_DOMAIN: '"' + process.env.CLIENT_DOMAIN + '"' ,
+  CLIENT_CUSTOM_DOMAIN: '"' + process.env.CLIENT_CUSTOM_DOMAIN + '"' ,
   AUTH0_CALLBACK_REDIRECT: '"' + process.env.AUTH0_CALLBACK_REDIRECT + '"',
   SCOPE: '"full_access"',
   AUTH0_AUDIENCE: '"' + process.env.AUTH0_AUDIENCE + '"',
