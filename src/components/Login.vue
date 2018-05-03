c0a3fe4883b8604357b801915770a4a4b417a3c7
Sebastian <tykiblood1992@hotmail.com>
2018-05-03 01:11:22 -0500

    test

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="general-container">
+  <div>
+    hola!!
   </div>
 </template>
 <script>
 import { login } from '../../utils/auth'
-import { setup } from '../../utils/hotjar'
 
 export default {
-  beforeCreate: function () {
-    setup(this)
+  beforeCreate () {
     login()
   }
 }
 </script>
-<style scoped>
-</style>
